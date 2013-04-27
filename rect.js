21808b38d247c83f08a0c8f57d0819daa6d7423c
Ethanmn <Ethanmn@hotmail.com>
2013-04-27 15:16:35 -0700

    rectangle

--- rect.js
+++ rect.js
@@ -4,14 +4,21 @@ var Rectangle = function()
 	this.y = 0,
 	this.width = 10,
 	this.height = 10,
+	this.red = 0,
+	this.green = 0,
+	this.blue = 0,
 	
 	this.collide = function(other)
 	{
-	
+	   
 	},
 	
 	this.draw = function()
 	{
-		
+	   var canvas = document.getElementById("canvas");
+	   var ctx = canvas.getContext('2d');
+	   ctx.fillRect(x, y, width, height);
+	   ctx.fillStyle = "rgba(" + red + "," + green + "," + blue + "," + alpha + ")"; 
+	   
 	}
 };
