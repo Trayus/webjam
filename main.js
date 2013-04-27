4937b486c90a8d66a4cf2b8201f05f6bd4ab910a
Trayus <timotheyadam@gmail.com>
2013-04-27 14:55:25 -0700

    Starting framework stuff

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 function draw()
 {
-	var canvas = document.getElementById("canvas");
-	var ctx = canvas.getContext('2d');
-	ctx.fillRect(25,25,100,100);
 	
-	console.log("hai");
+	
 }
 
-draw();
+function gameloop()
+{
+	//console.log("hello");
+}
+
+setInterval(gameloop, 50);
+
---
+++ rect.js
@@ -0,0 +1,17 @@
+var Rectangle = function()
+{
+	this.x = 0,
+	this.y = 0,
+	this.width = 10,
+	this.height = 10,
+	
+	this.collide = function(other)
+	{
+	
+	},
+	
+	this.draw = function()
+	{
+		
+	}
+};
