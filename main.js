cd94b945454e727c2fc641dbd1490fddf4303f86
Ethanmn <Ethanmn@hotmail.com>
2013-04-27 14:58:45 -0700

    uh

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ function draw()
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext('2d');
 	ctx.fillRect(25,25,100,100);
-	
+    ctx.clearRect(45,45,60,60);
+    ctx.strokeRect(50,50,50,50);
 	console.log("hai");
 }
 
