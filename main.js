8bb2c6c2d786f69b51e15d648652a325bb03103f
Ethanmn <Ethanmn@hotmail.com>
2013-04-27 17:20:30 -0700

    map, rain.js

--- main.js
+++ main.js
@@ -19,11 +19,12 @@ function gameloop()
 }
 
 /** initiation code **/
-map.push(new Rectangle(10, 10, 100, 100, 250, 250, 250));
-map.push(new Rectangle(200, 50, 10, 50, 250, 250, 250));
+
+map.push(new Rectangle(190, 510, 800, 40, 250, 250, 250));
+map.push(new Rectangle(300, 350, 200, 10, 250, 250, 250));
+map.push(new Rectangle(650, 250, 200, 10, 250, 250, 250));
+
 console.log(map);
-
-
 
 setInterval(gameloop, 50);
 draw();
